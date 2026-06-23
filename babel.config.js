74fc007b8039bca1f0d47827cb558edbf46726e0
kylodelgado <kylo.delgado@gmail.com>
2026-06-23 03:04:50 -0400

    feat: migrate to development builds with expo-dev-client, add reanimated v4 dependencies, and configure project bundle identifier

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // react-native-worklets/plugin powers Reanimated v4 and must be listed last.
+    plugins: ['react-native-worklets/plugin'],
   };
 };
